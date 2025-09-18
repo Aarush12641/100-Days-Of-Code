643543d9ec74e30fdb7a3ebc983dc639d35b08f5
Aarush <your.email@example.com>
2025-09-18 19:39:34 +0530

    done

---
+++ Non-Leet-Code/CheckPrime.java
@@ -0,0 +1,14 @@
+public class CheckPrime {
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 29;
+        System.out.println(num + " is prime? " + isPrime(num));
+    }
+}
